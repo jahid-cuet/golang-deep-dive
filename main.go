390113f26231b2c7a36c6fa11f64186b191a0a40
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2026-02-14 19:26:46 +0600

    feat: data-type and variablesn

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
-package main 
+package main
+
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!
+	
+  /*
+  int
+  float32
+  float64
+  string
+  bool
+  */
+
+//   var a int =10
+a := 10
+a=11 //No need to use colon 2nd time
+fmt.Println(a)
+
+
 }
