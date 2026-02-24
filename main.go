f2523c07467f98521e6881fccaaec1f1e483b683
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2026-02-24 23:27:44 +0600

    feat: if else and switch

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("Hello, World!
+	// fmt.Println("Hello, World!")
 	
   /*
   int
@@ -18,5 +18,33 @@ a := 10
 a=11 //No need to use colon 2nd time
 fmt.Println(a)
 
+//data types--> Numeric, String, Boolean
 
+// if else and switch case
+  age := 1
+  if age >= 18 {
+    fmt.Println("You are an adult.")
+  } else if age >= 13 {
+    fmt.Println("You are a minor.")
+  } else {
+    fmt.Println("You are a child.")
+  }
+
+
+  is_prime := true
+  if is_prime {
+    fmt.Println("The number is prime.")
+  } else {
+    fmt.Println("The number is not prime.")
+  }
+
+  day := "Monday"
+  switch day {
+  case "Monday":
+    fmt.Println("It's the start of the week.")
+  case "Friday":
+    fmt.Println("It's the end of the week.")
+  default:
+    fmt.Println("It's a regular day.")
+  }
 }
